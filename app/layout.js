d3bd82bcb79ef7f68ff7c017d84b188f023cf4a5
Jan Van Proeyen <j.vanproeyen@tag-team.be>
2023-08-17 23:02:06 +0200

    Refresh page every 5 sec

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta http-equiv="refresh" content="5" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
