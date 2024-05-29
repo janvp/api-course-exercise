cfa8686c518a68cb5210b61f8f138d1f272bb8f2
Jan Van Proeyen <j.vanproeyen@tag-team.be>
2024-05-29 10:08:12 +0200

    Fetch changes in db on interval

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from 'next/image'
+import React, { useState, useEffect } from 'react';
 import styles from './page.module.css'
 import Product from './product'
 let db = require('../db.json')
@@ -6,6 +9,29 @@ let db = require('../db.json')
 export default function Home() {
   const findUser = userId => db.users.find(user => user.id == userId);
   const findImage = imageId => db.images.find(image => image.id == imageId);
+
+  const [db, setDb] = useState({products: []});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api-course.tag-team.be/db');
+        const dbContents = await response.json();
+        setDb(dbContents);
+      } catch (error) {
+        console.error('Error fetching the JSON file:', error);
+      }
+    };
+
+    // Fetch data immediately on component mount
+    fetchData();
+
+    // Set up the interval to fetch data every 3 seconds
+    const intervalId = setInterval(fetchData, 3000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <main className={styles.main}>
